16b22404b11fc1e77e7fb56d862a7279ee294d15
daveinthahood <davidesimone96@outlook.it>
2023-11-06 10:44:05 +0100

    FIX: spacing buttons

--- src/Components/WaitingRoom.jsx
+++ src/Components/WaitingRoom.jsx
@@ -18,7 +18,7 @@ function WaitingRoom() {
         </div>
         <h1 className='text-white mt-4 text-4xl'> Issa ComeBack! </h1>
         <p className='text-white mt-4 text-2xl p-2'> Issa sta tornando con molte novità! <br /> A breve lanceremo questo sito e tutto avrà una nuova veste. </p>
-        <div className=' mt-8 flex justify-center text-center gap-5 decoration-none'>
+        <div className=' mt-8 flex justify-center text-center gap-3 decoration-none'>
             <a href="https://open.spotify.com/show/5tWQcXyvspOyZvaoEAC268" className='text-white border-1 rounded-xl bg-green-500 p-2'> Spotify </a>
             <a href="https://www.instagram.com/issapodcast_/" className='text-white border-1 rounded-xl bg-green-500 p-2'> Instagram </a>
             <a href="https://www.tiktok.com/@issapodcast" className='text-white border-1 rounded-xl bg-green-500 p-2'> TikTok </a>
