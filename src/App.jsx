16a62383d379d8f5948c517f86c0ecea1ab5d4a5
daveinthahood <davidesimone96@outlook.it>
2023-11-06 11:14:36 +0100

    add analitycs vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import WaitingRoom from './Components/WaitingRoom'
-
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <WaitingRoom />
+      <Analytics />
     </>
   )
 }
